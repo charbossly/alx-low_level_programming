d7c1f1d33e6423d296a97dedcc12af97453607aa
charbossly <assogbacharbossly@gmail.com>
2022-12-09 23:40:25 +0100

    Correct 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,14 @@
 int main(void)
 {
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
-	putchar(i%10 + '0');
+	putchar(i % 10 + '0');
 	if (i != 9)
 	{
 	putchar(',');
-	putchar(' ');	
+	putchar(' ');
 	}
 	}
 	putchar('\n');
