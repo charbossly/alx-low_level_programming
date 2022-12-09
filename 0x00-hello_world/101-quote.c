63b2ad0318bb56d46a1670b069515910a7eec222
charbossly <assogbacharbossly@gmail.com>
2022-12-09 03:28:08 +0100

    Add 101-quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
 * main - This is a description
-* Return: 0
+* Return: 1
 */
 int main(void)
 {
 puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-return (0);
+return (1);
 }
