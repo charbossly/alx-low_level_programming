b795a9ed1d18704b7481269ac0f9597978900cef
charbossly <assogbacharbossly@gmail.com>
2022-12-09 22:58:54 +0100

    Correct 3-print_alphabets.c  file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - This is a description
+* Return: 0
+*/
+int main(void)
+{
+	char n;
+	int i;
+	for (n = 'a'; n != 'z'; n++)
+	{
+	putchar(n);
+	}
+	for (n = 'A'; n != 'Z'; n++)
+        {
+        putchar(n);
+        }
+
+	putchar('$');
+	return (0);
+}
