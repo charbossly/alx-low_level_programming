b7ab87b59fc5d0967ac5ba1cbc64c367c4ef869b
charbossly <assogbacharbossly@gmail.com>
2022-12-09 03:01:20 +0100

    Add 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,6 @@
 */
 int main(void)
 {
-printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %d byte(s)", sizeof(char), sizeof(int), sizeof(long), sizeof(double), sizeof(float));
+printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %d byte(s)\n", sizeof(char), sizeof(int), sizeof(long), sizeof(double), sizeof(float));
 return (0);
 }
