879bbce037ba0b89a10123fceb7662a3af047996
charbossly <assogbacharbossly@gmail.com>
2022-12-09 21:45:27 +0100

    Add 2-print_alphabet.c  file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - This is a description
+* Return: 0
+*/
+int main(void)
+{
+	char n[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	int i;
+	for (i = 0; i < 26; i++)
+	{
+	putchar(n[i]);
+	}
+	putchar('\n');
+	return (0);
+}
