2916000cbefcdc9048c910b881364d342a1ad102
charbossly <assogbacharbossly@gmail.com>
2022-12-14 02:17:31 +0100

    Add 3-islower.c file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - check the code
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+
+	if( islower(c) > 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return 0;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 void print_alphabet(void);
 int _putchar(char c);
 void print_alphabet_x10(void);
+int _islower(int c);
 
 #endif
