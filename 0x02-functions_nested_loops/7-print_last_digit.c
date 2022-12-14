cc982b142ce42df4aa13c5bf14380e771ab8d874
charbossly <assogbacharbossly@gmail.com>
2022-12-14 03:56:23 +0100

    Add 9-times_table.c,8-24_hours.c and 7-print_last_digit.c files

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
