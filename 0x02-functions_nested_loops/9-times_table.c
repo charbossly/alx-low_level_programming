90561af2e7774ae174760bf27ae6eda49f8b3711
charbossly <assogbacharbossly@gmail.com>
2022-12-14 22:09:23 +0100

    Add 9-times_table.c file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,11 +8,26 @@
 
 void times_table(void)
 {
-	char n;
+	int n, m;
 
-	for (n = 'a'; n <= 'z'; n++)
+	for (n = 0; n < 10; n++)
 	{
-		_putchar(n);
+		for (m = 0; m < 10; m++)
+	        {
+			if (n * m != 0 && n * m >= 10)
+			{
+				_putchar('.');
+			}
+			if (n * m != 0 && n * m < 10) 
+                        { 
+                                _putchar('.');
+				_putchar('.');
+                        }
+			_putchar(n * m + '0');
+			if (m != 9)
+			{
+				_putchar(',');
+			}
+		}
 	}
-	_putchar('\n');
 }
