0c77ed056069f5478d2b479809f990e714e0cd6b
charbossly <assogbacharbossly@gmail.com>
2022-12-14 23:58:50 +0100

    Correct 8-24_hours.c file "

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,24 +13,24 @@ void jack_bauer(void)
 	for (a = 0; a < 3; a++)
 	{
 		for (b = 0; b < 10 ; b++)
-        	{
+		{
 			if (a == 2 && b > 3)
 			{
 				break;
 			}
 			for (c = 0; c < 6; c++)
-        		{
+			{
 				for (d = 0; d < 10; d++)
-        			{
+				{
 					_putchar(a + '0');
 					_putchar(b + '0');
 					_putchar(':');
 					_putchar(c + '0');
 					_putchar(d + '0');
 					_putchar('\n');
-        			}
-        		}
-		}	
+				}
+			}
+		}
 	}
 	_putchar('\n');
 }
