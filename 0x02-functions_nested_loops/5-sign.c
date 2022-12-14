21f21021abb2ff307ee9f3fe555e8ad8282f889b
charbossly <assogbacharbossly@gmail.com>
2022-12-14 03:53:14 +0100

    Correct 11-print_to_98.c file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,14 +16,14 @@ int print_sign(int n)
 	else
 	{
 		if (n == 0)
-        	{
-               		_putchar('0');
-                	return (0);
-        	}
+		{
+			_putchar('0');
+			return (0);
+		}
 		else
 		{
-                	_putchar('-');
-                	return (-1);
+			_putchar('-');
+			return (-1);
 		}
-        }
+	}
 }
