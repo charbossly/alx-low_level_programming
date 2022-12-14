9b98ac0fa1b19eeadfcf8082fdbe753a93731b25
charbossly <assogbacharbossly@gmail.com>
2022-12-14 01:25:24 +0100

    Add correct 1-alphabet.c file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,19 +8,21 @@
  * Return: Always 0.
  */
 
-void print_alphabet()
-{
-	char n;
-
-	for (n = 'a'; n <= 'z'; n++)
-	{
-	putchar(n);
-	}
-	putchar('\n');
-}
+void print_alphabet();
 
 int main(void)
 {
 print_alphabet();
 return (0);
+}
+
+void print_alphabet()
+{
+        char n;
+
+        for (n = 'a'; n <= 'z'; n++)
+        {
+        putchar(n);
+        }
+        putchar('\n');
 }
