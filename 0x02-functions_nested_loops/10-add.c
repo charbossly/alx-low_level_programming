dbcb26f62dcfa2666da68f912a23f1f77c7914ff
charbossly <assogbacharbossly@gmail.com>
2022-12-14 03:03:26 +0100

    Add 11-print_to_98.c and 10-add.c files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - check the code
+ *
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+	return a + b;	
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_to_98 - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (i = n; i <=98; i++)
+		{
+			_putchar(i+'0');
+			if (i !== 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >=98; i--) 
+                { 
+                        _putchar(i+'0');
+                        if (i !== 98)
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+                        }
+                }
+	}
+	_putchar('\n');
+}
