38a64fdcd22fba33d1085459b99c537ef3c1b052
charbossly <assogbacharbossly@gmail.com>
2022-12-15 22:52:39 +0100

    Update 6-main.c file

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -16,7 +16,7 @@ void print_line(int n)
         {
                 for (i = 0; i < n; i++)
                 {
-                        _putchar('_');
+                        _putchar('');
                 }
         }
 	_putchar('\n');
