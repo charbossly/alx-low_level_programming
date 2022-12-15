5e9f76fac64d78ff96948b95f60139425160abdb
charbossly <assogbacharbossly@gmail.com>
2022-12-15 21:55:47 +0100

    Add 6-main.c file

---
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*  print_line - This is a description
+*/
+void print_line(int n)
+{
+	int i;
+
+	if (i < 0 || i == 0)
+	{
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
