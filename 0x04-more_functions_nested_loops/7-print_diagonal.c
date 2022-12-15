d118a9e1cc3d58869692c1e8dbe0c1555c6080ac
charbossly <assogbacharbossly@gmail.com>
2022-12-16 00:26:10 +0100

    Update 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_diagonal - This is a description
+* @n: Description of parameter n
+*/
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n - 1; i++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\');
+	}
+	_putchar('\n');
+}
