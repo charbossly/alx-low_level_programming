2457a18009f78974dda0231bc03c3152b2843cbf
charbossly <assogbacharbossly@gmail.com>
2022-12-15 23:16:31 +0100

    Update 6-main.c file

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,15 +4,12 @@
 * print_line - This is a description
 * @n: Description of parameter n
 */
+
 void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
-        {
-                _putchar('');
-        }
-        else
+	if (n > 0)
         {
                 for (i = 0; i < n; i++)
                 {
