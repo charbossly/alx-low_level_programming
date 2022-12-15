66e8c692848a0a896e051f1cf4d8c875ca74ac03
charbossly <assogbacharbossly@gmail.com>
2022-12-15 20:40:05 +0100

    Add 0-isupper.c file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* _isupper - This is a description
+* Return: 0 if no upper
+*/
+int _isupper(int c)
+{
+	if (isupper(c) != 0)
+	{
+		return (1);
+	}
+	}
+	return (0);
+}
