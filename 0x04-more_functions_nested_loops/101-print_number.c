3247dc49bcba5935460e2d53b1b657e8e6d8144a
charbossly <assogbacharbossly@gmail.com>
2022-12-16 15:12:04 +0100

    Correct 9-fizz_buzz.c file

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_square - This is a description
+* @size: Description of parameter size 
+*/
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; size++)
+			{
+				_putchar("#");
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -28,7 +28,10 @@ int main(void)
 		{
 			printf("%d", i);
 		}
-		printf(" ");
+		if (i < 100)
+		{
+			printf(" ");
+		}
 	}
 	printf("\n");
 	return (0);
