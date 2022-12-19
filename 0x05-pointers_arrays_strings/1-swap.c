16c142ee24dc1b2b7d742f3765677cb1ba04f859
charbossly <assogbacharbossly@gmail.com>
2022-12-19 23:44:00 +0100

    Add 1-swap.c file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* wap_int- This is a description
+* @a: Description of parameter a
+* @b: Description of parameter b 
+*/
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
