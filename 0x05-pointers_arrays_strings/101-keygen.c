e9b9a4da006ca5c5ed33ec8a7e4a49986ba2690b
charbossly <assogbacharbossly@gmail.com>
2022-12-20 20:46:01 +0100

    Add 101-keygen.c file

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - This is a description
+* Return: 0 Always
+*/
+int main(void) {
+    int i;
+
+	srand(time(NULL));
+     
+    
+	char abc[26]="abcdefghijklmnopqrstuvwxyz";
+    char newabc[8];
+    for (i = 0; i < 12; ++i) {
+        newabc[i] = abc[rand() % (sizeof(abc) - 1)];
+        printf("%c ", newabc[i]);
+    }
+    newabc[12] = 0; 
+    return 0;
+}
