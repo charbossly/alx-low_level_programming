e27000c8d50c6f960523c46d382ee8dfa6d1c8ee
charbossly <assogbacharbossly@gmail.com>
2022-12-20 21:35:04 +0100

    Add 100-atoi.c file

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,16 +1,43 @@
 #include "main.h"
+#include <string.h>
 
 /**
-* puts_half - This is a description
-* @str: Description of parameter str
+* _atoi - This is a description
+* @s: Description of parameter s
+* Return: integer
 */
-void puts_half(char *str)
+int _atoi(char *s);
 {
-	int i;
-
-	for ( i = (strlen(str) - 1 / 2); i < strlen(str); i++)
+	int i, d, n, len, f, digit;
+	
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+	
+	while (s[len] != '\0')
+		len++;
+	while (i<len && f ==0)
 	{
-		_putchar(*(str+i));
+		if (s[i] == '-')
+			++d;
+		if (s[i] >= '0' && s[i] <= 9)
+		{
+			digit = s[i] - '0';
+			if (d%2)
+				digit = -digit;
+			n = n*10+digit;
+			f = 1;
+			if (s[i+1] < '0' || s[i+1] > '9')
+				break;
+			f = 0;
+		}			
+		i++;
 	}
-	_putchar('\n');
+	if (f==0)
+		return (0);
+
+	return (n);
 }
