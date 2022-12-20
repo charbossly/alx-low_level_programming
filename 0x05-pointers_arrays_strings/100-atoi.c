760c4e7acad5ff28ab6b5285b4aa0ddae1abc6fb
charbossly <assogbacharbossly@gmail.com>
2022-12-20 21:39:52 +0100

    Add 100-atoi.c file[B

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -18,26 +18,35 @@ int _atoi(char *s);
 	digit = 0;
 	
 	while (s[len] != '\0')
+	{
 		len++;
+	}
 	while (i<len && f ==0)
 	{
 		if (s[i] == '-')
+		{
 			++d;
+		}
 		if (s[i] >= '0' && s[i] <= 9)
 		{
 			digit = s[i] - '0';
 			if (d%2)
+			{
 				digit = -digit;
+			}
 			n = n*10+digit;
 			f = 1;
 			if (s[i+1] < '0' || s[i+1] > '9')
+			{
 				break;
+			}
 			f = 0;
-		}			
+		}
 		i++;
 	}
 	if (f==0)
+	{
 		return (0);
-
+	}
 	return (n);
 }
