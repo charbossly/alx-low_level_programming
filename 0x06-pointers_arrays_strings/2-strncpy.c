3ed03f87b5a2356bf2a10f3171c5a50271303002
charbossly <assogbacharbossly@gmail.com>
2022-12-23 00:03:32 +0100

    Add 2-strncpy.c file

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strncpy - This is a description
+* @dest: Description of parameter dest
+* @src: Description of parameter src
+* @n: Description of parameter n
+* Return: string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = src[i];
+	}
+	return (dest);
+}
