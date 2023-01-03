b891d5f7a387f6b6f3fd34d587c40868818e51b4
charbossly <assogbacharbossly@gmail.com>
2023-01-03 01:53:45 +0100

    Add 100-set_string.c file

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* set_string - This is a description
+* @s: Description of parameter s
+* @to: Description of parameter to
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
