f501d2d5e9d0590d041d28bce19b99b184a905dd
charbossly <assogbacharbossly@gmail.com>
2023-01-05 01:55:49 +0100

    Add 6-is_prime_number.c,5-sqrt_recursion.c and 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,9 +7,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_putchar(*(s - 1));
-		_print_rev_recursion(s - 1);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
