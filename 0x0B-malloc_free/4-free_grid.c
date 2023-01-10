96829580779e1d8a6889bb9fb213c4c8f518a9cc
CharbelAssogba <69874512+charbossly@users.noreply.github.com>
2023-01-11 00:57:51 +0100

    Update 2-str_concat.c  file

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * tall - the code
+ * @s: parameter s
+ * Return: Always 0.
+ */
+int tall(char *s)
+{
+	int i = 0;
+
+	while (*s)
+	{
+		i++;
+		s++;
+	}
+
+	return (i);
+}
+/**
+ * str_concat - the code
+ * @s1: parameter s1
+ * @s2: parameter s2
+ * Return: Always 0.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int height = 0;
+	int i = 0;
+	int j = 0;
+
+	height = tall(s1) + tall(s2);
+	s = malloc(sizeof(*s) * height + 1);
+	if (!s)
+	{
+		return (NULL);
+	}
+	while (*s1 && s1 != NULL)
+	{
+		s[i] = *s1;
+		i++;
+		s1++;
+	}
+	while (*s2 && s2 != NULL)
+	{
+		s[i + j] = *s2;
+		j++;
+		s2++;
+	}
+	s[i + j] = '\0';
+	return (s);
+	free(s);
+}
