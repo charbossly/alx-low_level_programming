8cfd8c28ec160c61d133cac2ea55bd81f888ce4f
CharbelAssogba <69874512+charbossly@users.noreply.github.com>
2023-01-10 22:37:18 +0100

    Add 0-create_array.c, 1-strdup.c and 2-str_concat.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * create_array - the code
+ * @size: parameter size
+ * @c: parameter c
+ * Return: Always 0.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i = 0;
+
+	s = malloc(sizeof(*s) * size);
+	if (s == NULL)
+	{
+		return (s);
+	}
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	return (s);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * tall - the code
+ * @s: parameter s
+ * Return: Always 0.
+ */
+int tall(char *s)
+{
+	int i = 0;
+
+	while(*s)
+	{
+		i++;
+		s++;
+	}
+	  
+        return (i);
+}
+/**
+ * _strdup - the code
+ * @str: parameter str
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int height = 0;
+	int i = 0;
+
+	height = tall(str);
+	s = malloc(sizeof(*s) * height + 1);
+	while(*str)
+	{
+		s[i] = *str;
+		i++;
+		str++;
+	}
+	return (s);
+}
