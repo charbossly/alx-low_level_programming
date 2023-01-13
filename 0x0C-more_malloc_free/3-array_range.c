dc36f9e0f8285a7c99b47f7811403690aa282437
CharbelAssogba <69874512+charbossly@users.noreply.github.com>
2023-01-13 01:42:28 +0100

    Add 3-array_range.c, 2-calloc.c and 1-string_nconcat.c file

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * array_range - creates an array of integers
+ * @min: minimum value
+ * @max: maximum value
+ *
+ * Return: the pointer to the newly created array
+ */
+int *array_range(int min, int max)
+{
+	int *array;
+	int i;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	array = malloc(sizeof(int) * (max - min + 1));
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0 ; min <= max ; min++, i++)
+	{
+		array[i] = min;
+	}
+	return (array);
+}
