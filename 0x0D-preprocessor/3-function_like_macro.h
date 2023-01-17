09e058a0a32d031318b7390638d51ce8e0f2e1c4
CharbelAssogba <69874512+charbossly@users.noreply.github.com>
2023-01-17 02:15:42 +0100

    Add 0x0D-preprocessor files

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+fndef absolute_value
+#define absolute_value
+#define ABS(x) (((x) < 0) ? (x) * (-1) : (x))
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,5 @@
+
+#ifndef sum_numbers
+#define sum_numbers
+#define SUM(x, y) ((x) + (y))
+#endif
