27abcb9be51b4f44c6cb76258ba4ee31f62d1c10
charbossly <assogbacharbossly@gmail.com>
2023-01-26 23:59:04 +0100

    Add file

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,43 +1,99 @@
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 #include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+
 
 /**
- * add_node_end - adds a new node at the end
- * of a list_t list.
- * @head: head of the linked list.
- * @str: string to store in the list.
- * Return: address of the head.
+ * _strlen - function to get length of a string
+ * @s: string
+ *
+ * Return: Length of a array of characters
  */
+int _strlen(char *s)
+{
+	int i;
 
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory, which
+ * contains a copy of the string given as a parameter.
+ * @str: string to copy
+ *
+ * Return: Pointer
+ */
+char *_strdup(const char *str)
+{
+	int l = 0, i;
+	char *s;
+
+	if (str == NULL)
+		return (0);
+
+	while (*(str + l))
+		l++;
+
+	s = malloc(sizeof(char) * l + 1);
+
+	if (s == 0)
+		return (0);
+
+	for (i = 0; i <= l; i++)
+		*(s + i) = *(str + i);
+	return (s);
+}
+
+/**
+ * add_node_end - adds a new node to a singly linked list at the end of it
+ * @head: pointer to head of the singly linked list_t
+ * @str: string to add to the new node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *temp;
-	size_t nchar;
+	char *tmp;
+	list_t *tmph;
+	list_t *new_node = malloc(sizeof(list_t));
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
+	if (new_node == 0)
+		return (0);
 
-	new->str = strdup(str);
-
-	for (nchar = 0; str[nchar]; nchar++)
-		;
-
-	new->len = nchar;
-	new->next = NULL;
-	temp = *head;
-
-	if (temp == NULL)
+	tmph = *head;
+	if (str == 0)
 	{
-		*head = new;
+		new_node->str = 0;
+		new_node->len = 0;
 	}
 	else
 	{
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = new;
+		tmp = _strdup(str);
+		if (tmp == 0)
+		{
+			free(new_node);
+			return (0);
+		}
+		new_node->str = tmp;
+		new_node->len = _strlen(tmp);
 	}
+	new_node->next = 0;
 
-	return (*head);
+	if (tmph == 0)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		while (tmph->next != 0)
+			tmph = tmph->next;
+		tmph->next = new_node;
+	}
+	return (new_node);
 }
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,21 +1,15 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
-
 /**
- * free_list - frees a list.
- * @head: a list that enters to the function.
+ * free_list - frees a list
+ * @head: head of the linked list
+ *
  */
 void free_list(list_t *head)
 {
-list_t *temp;
-
-	temp = head;
-	while (temp != NULL)
+	if (head != NULL)
 	{
-		free(temp->str);
-		free(temp);
-		temp = temp->next;
+		free(head->str);
+		free_list(head->next);
 	}
+	free(head);
 }
