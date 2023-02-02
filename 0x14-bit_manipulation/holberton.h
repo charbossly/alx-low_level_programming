b4c784932d48aa82819fd7af626084de8dca3313
charbossly <assogbacharbossly@gmail.com>
2023-02-02 23:13:13 +0100

    Add 0x14-bit_manipulation folder

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,11 +1,43 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-int _putchar(char c);
-unsigned int binary_to_uint(const char *b);
-void print_binary(unsigned long int n);
-int get_bit(unsigned long int n, unsigned int index);
-int set_bit(unsigned long int *n, unsigned int index);
-int clear_bit(unsigned long int *n, unsigned int index);
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int get_endianness(void);
+
+/* _putchar.c */
+int _putchar(char);
+
+/* 1-alphabet.c */
+void print_alphabet(void);
+
+/* 2-print_alphabet_x10.c */
+void print_alphabet_x10(void);
+
+/* 3-islower.c */
+int _islower(int);
+
+/* 4-alpha.c */
+int _isalpha(int);
+
+/* 5-sign.c */
+int print_sign(int);
+
+/* 6-abs.c */
+int _abs(int);
+
+/* 7-print_last_digit.c */
+int print_last_digit(int);
+
+/* 8-24_hours.c */
+void jack_bauer(void);
+
+/* 9-times_table.c */
+void times_table(void);
+
+/* 10-add.c */
+int add(int, int);
+
+/* 11-print_to_98.c */
+void print_to_98(int);
+
+/* 100-times_table.c*/
+void print_times_table(int);
+
 #endif
