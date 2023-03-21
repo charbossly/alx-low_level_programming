71c1b3906f6512df7910dde9b9d89fa7ce456fd3
CharbelAssogba <69874512+charbossly@users.noreply.github.com>
2023-03-21 01:14:03 +0100

    Add files via upload

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -qO /tmp/jackpot.so https://github.com/jzamora5/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/jackpot.so
-export LD_PRELOAD=/tmp/jackpot.so
+wget -P /tmp https://github.com/sidneyriffic/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
+export LD_PRELOAD=/tmp/putshack.so
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,6 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-#include <stdlib.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* HOLBERTON_H */
+#endif /* _MAIN_H_ */
